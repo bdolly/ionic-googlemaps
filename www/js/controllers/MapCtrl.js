416110c89a95834c45ef35c124318fbcd9372a55
Ben Dolly <ben@1trickpony.com>
2016-04-29 08:39:37 -0400

    MapCtrl: refactored to update ui.knob properly on promise success

--- www/js/controllers/MapCtrl.js
+++ www/js/controllers/MapCtrl.js
@@ -5,7 +5,7 @@ var controllersModule = require('./_index');
 /**
  * @ngInject
  */
-function MapCtrl($rootScope, $scope, $log, $timeout, $ionicLoading, AppSettings, GMap, LocationsService ) {
+function MapCtrl($rootScope, $scope, $log, $ionicLoading, AppSettings, GMap, LocationsService) {
 
     $log = $log.getInstance("MapCtrl");
 
@@ -17,27 +17,41 @@ function MapCtrl($rootScope, $scope, $log, $timeout, $ionicLoading, AppSettings,
     vm.gmap = new GMap({zoom:13, radius:vm.this_radius});    
     vm.gmap.height = 625;
     vm.markers = [];    
-    vm.clearMarkers = function(markers){
-       for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(null);
-        }
-    }
-    
     // vm.gmap.plotLocations();
     
     vm.locations_by_distance = [];
+
+    vm.knob ={
+              value: 0,
+              options:{ dynamicOptions: true },
+            };
+
     
+    var setKnobValue = function(val) {
+      // set ui.knob options here
+      // this tricks the knob into updating the value correctly
+          vm.knob ={
+              value:val,
+              options:{
+                size: 200,
+                min:  1,
+                max:  10000,
+                step: 1,
+                barColor: '#5BC01E',
+                trackColor: '#212121',
+                trackWidth: 15,
+                barWidth: 30,
+                subText: {
+                  enabled: true,
+                  text: 'meter radius'
+                }
+              }
+            };
+    };
+   
 
-    // get all locations around specified area
-      LocationsService
-        .get({radius: vm.this_radius, 
-              lat:    currentCenter.latitude, 
-              lng:    currentCenter.longitude})
-        .then(function(locations) {
-
-          // sort locations by distnace 
-          vm.locations_by_distance = _.sortBy(locations, 'distancefromlocation');
-          vm.locations_by_distance.map(function(locate){
+    var setMarkers = function(locations) {
+      locations.map(function(locate){
             var marker = vm.gmap.dropMarker({
                         center: {
                             lat: parseFloat(locate.lat),
@@ -48,67 +62,75 @@ function MapCtrl($rootScope, $scope, $log, $timeout, $ionicLoading, AppSettings,
                         opacity:   0.125
                       });
                     
-                    vm.markers[locate._id] = marker;
+                vm.markers[locate._id] = marker;
           });
+
+        return vm.markers;
+    };
+
+      
+
+    // get all locations around specified area
+      LocationsService
+        .get({radius: vm.this_radius, 
+              lat:    currentCenter.latitude, 
+              lng:    currentCenter.longitude})
+        .then(function(locations) {
+
+          // sort locations by distnace 
+          vm.locations_by_distance = _.sortBy(locations, 'distancefromlocation');
+          setMarkers(vm.locations_by_distance);
 
           // set the locations radius and knob value to the closes loction to test
           vm.this_radius = _.first(vm.locations_by_distance).distancefromlocation;
           vm.locationsRadius = vm.gmap.setRadiusCircle({radius: vm.this_radius});
           vm.gmap.map.fitBounds(vm.locationsRadius.getBounds());
-
-          vm.knob.value = vm.this_radius;
-
-
-          // wathc for the knob value to change 
-          $scope.$watch("Map.knob.value", function(oldValue, newValue) {
-              vm.gmap.map.panTo({lat:$rootScope.currentPosition.latitude, lng:$rootScope.currentPosition.longitude});
-              vm.locationsRadius.setRadius(newValue);
-              // TODO: calculate proper zoom based on locationRadius
-              // vm.gmap.map.setZoom(newValue);
-              
-              
-
-              vm.locations_by_distance.map(function(locate){
-                    if( locate.distancefromlocation < newValue ){
-                      vm.markers[locate._id].setOpacity(1);
-                    }else {
-                      vm.markers[locate._id].setOpacity(0);
-                    }
-              });//vm.locations_by_distance.map
-
-
-          });///$scope.$watch "Map.knob.value",
-
-            
+          
+          setKnobValue(vm.this_radius)
+                    
         });//end .then
 
+
+
+
+
+
+
+         $scope.$watch("Map.knob.value", function(newValue, oldValue) {
+            $log.log("new Map.knob.value: "+newValue);
+            $log.log("old Map.knob.value: "+oldValue);
+
+            vm.locationsRadius.setRadius(newValue);
+            // vm.gmap.map.panTo({lat:$rootScope.currentPosition.latitude, lng:$rootScope.currentPosition.longitude});
+            var MapBoundsArr = _.toArray(vm.gmap.map.getBounds());
+            var RadiusBoundsArr = _.toArray(vm.locationsRadius.getBounds());
+            // TODO: check if RadiusBounds are within MapBounds
+            $log.log("MapBoundsArr: "+MapBoundsArr);
+            $log.log("RadiusBoundsArr: "+ RadiusBoundsArr );
+
+            // show/reveal locations inside radius
+            vm.locations_by_distance.map(function(locate){
+                  if( locate.distancefromlocation < newValue ){
+                    vm.markers[locate._id].setOpacity(1);
+                  }else {
+                    vm.markers[locate._id].setOpacity(0);
+                  }
+            });//vm.locations_by_distance.map
+
+
+        });///$scope.$watch "Map.knob.value"
+      
+    
+
+
         
         
-         vm.knob ={
-              value: vm.this_radius,
-              options:{
-                size: 200,
-                min:  1,
-                max:  10000,
-                step: 1,
-                
-                barColor: '#5BC01E',
-                trackColor: '#212121',
-                trackWidth: 15,
-                barWidth: 30,
-                subText: {
-                  enabled: true,
-                  text: 'meter radius'
-                },
-              }
-            };
-   
+         
             
             
 
   
-    
-
+  
 
   $rootScope.$on('marker:clicked', function(e, locale) {
     $scope.$apply(function() {
